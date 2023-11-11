d80a6624d3a10c9ac8dd5fe506b2397b777bd16f
Kirill Savin <kirsavin33@gmail.com>
2023-11-11 14:28:19 +0300

    Add static files

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
 
 def index(request):
-    return HttpResponse('Home page')
+    text = 'Это главная страница проекта Yatube'
+    template = 'posts/index.html'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Nothing yet {slug}')
+    text = 'Здесь будет информация о группах проекта Yatube'
+    template = 'posts/group_list.html'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
