de99b032b158b09280d06126c5f29c82b463f20e
Kirill Savin <kirsavin33@gmail.com>
2023-12-03 14:21:48 +0300

    Add paginator and decorators

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 
+from django.core.paginator import Paginator
+
+from django.contrib.auth.decorators import login_required
+
 from .models import Post, Group
 
 
 def index(request):
     text = 'Последние обновления на сайте'
     template = 'posts/index.html'
-    posts = Post.objects.all()[:10]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'text': text,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
 
+@login_required
 def group_posts(request, slug):
     text = 'Записи сообщества'
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:10]
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    # posts = group.posts.all()[:10]
     context = {
         'text': text,
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
