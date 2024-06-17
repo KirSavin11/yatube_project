645603040f5493024db9c18a62e3b0f25852a4f7
Kirill Savin <kirsavin33@gmail.com>
2024-06-17 23:52:48 +0300

    Add followers

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -76,3 +76,24 @@ class Comment(CreatedModel):
 
     def __str__(self):
         return self.text[:15]
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='follower',
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='following',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'user'],
+                name='unique_following'
+            )
+        ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_page
-from .models import Post, Group, get_user_model, Comment
+from .models import Post, Group, get_user_model, Comment, Follow
 
 from .forms import PostForm, CommentForm
 
 User = get_user_model()
+
+
+def paginator(request, posts):
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
 
 
 @cache_page(20)
@@ -42,14 +49,21 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts_list = Post.objects.filter(author=author)
+    page_obj = paginator(request, posts_list)
     template = 'posts/profile.html'
-    name = User.objects.get(username=username)
-    posts_list = Post.objects.filter(author=name)
-    post_count = Post.objects.filter(author=name).count()
+    post_count = Post.objects.filter(author=author).count()
+    if request.user.is_authenticated:
+        following = Follow.objects.filter(
+            user=request.user, author=author).exists
+    else:
+        following = False
     context = {
-        'username': username,
-        'full_name': name,
+        'author': author,
+        'page_obj': page_obj,
         'posts_list': posts_list,
+        'following': following,
         'post_count': post_count,
     }
     return render(request, template, context)
@@ -120,3 +134,34 @@ def add_comment(request, post_id):
         comment.post = post
         comment.save()
     return redirect('posts:post_detail', post_id=post_id)
+
+
+@login_required
+def follow_index(request):
+    posts = (
+        Post.objects
+        .select_related('author', 'group')
+        .filter(author__following__user=request.user))
+    page_obj = paginator(request, posts)
+    template = 'posts/follow.html'
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def profile_follow(request, username):
+    author = User.objects.get(username=username)
+    if request.user != author:
+        Follow.objects.create(user=request.user, author=author)
+    return redirect('posts:profile', username=author)
+
+
+@login_required
+def profile_unfollow(request, username):
+    author = User.objects.get(username=username)
+    is_follower = Follow.objects.filter(user=request.user, author=author)
+    if is_follower.exists():
+        is_follower.delete()
+    return redirect('posts:profile', username=author)
